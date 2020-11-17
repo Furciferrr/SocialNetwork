d32b1b2907f043de38b0d120f321a093f7c9979f
Vadim <3643766@mail.ru>
2020-11-17 21:49:39 +0300

    added route and navlink

---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Music.module.css';
+
+
+
+const Music = (props) => {
+    return (
+      <div className={classes.music}>
+        Music 
+      </div>
+    )
+}
+
+export default Music
