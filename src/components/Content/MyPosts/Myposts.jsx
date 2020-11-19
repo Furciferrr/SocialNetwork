099ead1dfa7a80c03a03c5dea6eb2e6df71d6c7f
Vadim <3643766@mail.ru>
2020-11-20 01:25:22 +0300

    added map to dialogs

--- src/components/Content/MyPosts/Myposts.jsx
+++ src/components/Content/MyPosts/Myposts.jsx
@@ -4,6 +4,10 @@ import Post from './Post/Post';
 
 
 const MyPosts = () => {
+  let postsData = [
+    {id: '1', message:'Hi, bro..,', likeNumb: '32', avaLink: 'https://i.pinimg.com/170x/a1/9b/83/a19b83023cdb650a17623bf6bf456245.jpg'},
+    {id: '1', message:'Hello world!', likeNumb: '20', avaLink: 'https://i.pinimg.com/170x/a1/9b/83/a19b83023cdb650a17623bf6bf456245.jpg'},
+  ]
     return (
           <div className={classes.posts}>My posts
               <div>
@@ -12,12 +16,13 @@ const MyPosts = () => {
                   </div>
                   <button>add post</button>
               </div>
-            <Post message='Hi, bro..,'
-                  likenumb="32"  
-                  avalink='https://i.pinimg.com/170x/a1/9b/83/a19b83023cdb650a17623bf6bf456245.jpg'/>
-            <Post message='Hello world'
-                  likenumb="20"
-                  avalink='https://sun9-17.userapi.com/c303205/g37357819/a_7a7e0de6.jpg?ava=1'/>
+            <Post message={postsData[0].message}
+                  likenumb={postsData[0].likeNumb}  
+                  avalink={postsData[0].avaLink}/>
+                  
+            <Post message={postsData[1].message}
+                  likenumb={postsData[1].likeNumb}
+                  avalink={postsData[1].avaLink}/>
           </div>
     )
 }
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -16,21 +16,39 @@ const Message = (props) => {
   <div className={classes.message}>{props.message}</div>
   )
 }
-
+ 
 const Dialogs = (props) => {
+  
+    let dialogData = [
+      {id: 1, name: 'Dimych'},
+      {id: 2, name: 'Masha'},
+      {id: 3, name: 'Sveta'},
+      {id: 4, name: 'Saha'},
+      {id: 5, name: 'Vadim'}
+    ]
+
+    let messagesData = [
+      {id: 1, message: 'Hi'},
+      {id: 2, message: 'How are you?'},
+      {id: 3, message: 'What the fuck?'}
+    ]
+
+    let dialogsElements = dialogData.map((element) => {
+      return(
+        <DialogItem name={element.name} id={element.id}/>
+      );
+    });
+
+
+    let messagesElements = messagesData.map(message => <Message message={message.message}/>);
+
     return (
       <div className={classes.dialogs}>
         <div className={classes.dialogItems}>
-            <DialogItem name="Dimych" id="1"/>
-            <DialogItem name="Masha" id="2"/>
-            <DialogItem name="Sveta" id="3"/>
-            <DialogItem name="Sasha" id="4"/>
-            <DialogItem name="Vadim" id="5"/>
+           {dialogsElements}
         </div>
         <div className={classes.messagesBlock}>
-            <Message message="Hi"/>
-            <Message message="How are you?"/>
-            <Message message="What the fuck?"/>
+           {messagesElements}
         </div>
       </div>
       
