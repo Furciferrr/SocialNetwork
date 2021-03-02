004463a8d142a5e20b0983c252fff671a8f40224
VadimS Job <romeo65@inbox.ru>
2021-03-02 14:42:16 +0300

    'addedErrorsPage'

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import classes from './News.module.css';
 
 
 
 const News = (props) => {
+
+  const but = useRef()
+
+  useEffect(()=>{
+    but.current.addEventListener('click', ()=>{
+      console.log('clicked me')
+    })
+  },[])
+
+
+
 
   let newsElem = props.news.map( n => {
     return (
@@ -19,9 +30,27 @@ const News = (props) => {
     return (
       <div>
         { newsElem }
+        <button ref={but}>push</button>
+        <EffectsDemoNoDependency/>
       </div>
      
     )
 }
 
+function EffectsDemoNoDependency() {
+  const [title, setTitle] = useState("default title");
+  const titleRef = useRef();
+  useEffect(() => {
+    console.log("useEffect");
+    document.title = title;
+  },[]);
+  const handleClick = () => setTitle(titleRef.current.value);
+  console.log("render");
+  return (
+    <div>
+      <input ref={titleRef} />
+      <button onClick={handleClick}>change title</button>
+    </div>
+  );
+}
 export default News
